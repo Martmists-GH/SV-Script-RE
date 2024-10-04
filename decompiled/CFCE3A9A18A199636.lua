50854619371125ad219b7b9e6625cbc485bb8f98
Martmists <martmists@gmail.com>
2024-10-04 12:16:14 +0200

    editorconfig and two files
    Signed-off-by: Martmists <martmists@gmail.com>

--- decompiled/CFCE3A9A18A199636.lua
+++ decompiled/CFCE3A9A18A199636.lua
@@ -1,12 +1,6 @@
-L55_1 = _ENV
-L56_1 = "CFCE3A9A18A199636"
-L57_1 = L15_1
-L57_1 = L57_1()
-L55_1[L56_1] = L57_1
-L68_1 = _ENV["CFCE3A9A18A199636"]
-L69_1 = "new"
+CFCE3A9A18A199636 = L15_1()
 
-function L70_1()
+function CFCE3A9A18A199636.new()
   local L0_2, L1_2, L2_2, L3_2
   L0_2 = L2_1
   L1_2 = CFCE3A9A18A199636
@@ -21,73 +15,31 @@ function L70_1()
   return L0_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCE3A9A18A199636"]
-L69_1 = "super"
+local KEY_id = 1
+local KEY_sortNum = 2
+local KEY_itemType = 3
 
-function L70_1(A0_2)
-  local L1_2
-  A0_2[3] = 13
-  A0_2[2] = 0
-  A0_2[1] = 0
+function CFCE3A9A18A199636.super(self)
+  self[KEY_itemType] = 13
+  self[KEY_sortNum] = 0
+  self[KEY_id] = 0
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCE3A9A18A199636"]
-L69_1 = "__name__"
-L70_1 = "CFCE3A9A18A199636"
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCE3A9A18A199636"]
-L69_1 = "prototype"
-L70_1 = L15_1
-L70_1 = L70_1()
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCE3A9A18A199636"]["prototype"]
-L69_1 = "F31CE6DDF7EF87630"
+CFCE3A9A18A199636.__name__ = "CFCE3A9A18A199636"
+CFCE3A9A18A199636.prototype = L15_1()
 
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2
-  L3_2 = A1_2
-  L2_2 = A1_2.fCD31E312
-  L4_2 = "Id"
-  L2_2 = L2_2(L3_2, L4_2)
-  A0_2[1] = L2_2
-  L3_2 = A1_2
-  L2_2 = A1_2.fCD31E312
-  L4_2 = "SortNum"
-  L2_2 = L2_2(L3_2, L4_2)
-  A0_2[2] = L2_2
-  L3_2 = A1_2
-  L2_2 = A1_2.fBAF32369
-  L4_2 = "ItemType"
-  L2_2 = L2_2(L3_2, L4_2)
-  L3_2 = L2_2
-  L2_2 = L2_2.f55728F9C
-  L2_2 = L2_2(L3_2)
-  A0_2[3] = L2_2
+function CFCE3A9A18A199636.prototype.F31CE6DDF7EF87630(self, A1_2)
+  self[KEY_id] = A1_2:fCD31E312("Id")
+  self[KEY_sortNum] = A1_2:fCD31E312("SortNum")
+  self[KEY_itemType] = A1_2:fBAF32369("ItemType"):f55728F9C()
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCE3A9A18A199636"]["prototype"]
-L69_1 = "F44E3303AD8B7397D"
-
-function L70_1(A0_2)
-  local L1_2
-  L1_2 = A0_2[2]
-  return L1_2
+function CFCE3A9A18A199636.prototype.F44E3303AD8B7397D(self)  -- GetSortNum
+  return self[KEY_sortNum]
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCE3A9A18A199636"]["prototype"]
-L69_1 = "F3701D40EBF2D4C18"
-
-function L70_1(A0_2)
-  local L1_2
-  L1_2 = A0_2[3]
-  return L1_2
+function CFCE3A9A18A199636.prototype.F3701D40EBF2D4C18(self)  -- GetItemType
+  return self[KEY_itemType]
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCE3A9A18A199636"]["prototype"]
-L69_1 = _ENV["CFCE3A9A18A199636"]
-L68_1.__class__ = L69_1
+CFCE3A9A18A199636.prototype.__class__ = CFCE3A9A18A199636
--- decompiled/CFD0B7F7DAE438AF7.lua
+++ decompiled/CFD0B7F7DAE438AF7.lua
@@ -1,12 +1,6 @@
-L55_1 = _ENV
-L56_1 = "CFD0B7F7DAE438AF7"
-L57_1 = L15_1
-L57_1 = L57_1()
-L55_1[L56_1] = L57_1
-L68_1 = _ENV["CFD0B7F7DAE438AF7"]
-L69_1 = "new"
+CFD0B7F7DAE438AF7 = L15_1()
 
-function L70_1()
+function CFD0B7F7DAE438AF7.new()
   local L0_2, L1_2, L2_2, L3_2
   L0_2 = L2_1
   L1_2 = CFD0B7F7DAE438AF7
@@ -21,98 +15,45 @@ function L70_1()
   return L0_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFD0B7F7DAE438AF7"]
-L69_1 = "super"
-
-function L70_1(A0_2)
+function CFD0B7F7DAE438AF7.super(self)
   local L1_2, L2_2, L3_2
-  A0_2[7] = 30
-  A0_2[6] = 30
-  L1_2 = _hx_tab_array
-  L2_2 = {}
-  L2_2.length = 0
-  L3_2 = 0
-  L1_2 = L1_2(L2_2, L3_2)
-  A0_2[3] = L1_2
-  L1_2 = A0_2[3]
-  L2_2 = L1_2
-  L1_2 = L1_2.resize
-  L3_2 = 5
-  L1_2(L2_2, L3_2)
-  L1_2 = cEF86D56B
-  L1_2 = L1_2.fA925EE56
-  L1_2 = L1_2()
-  L2_2 = L1_2
-  L1_2 = L1_2.f6E91F345
-  L3_2 = A0_2[6]
-  L1_2(L2_2, L3_2)
-  L1_2 = cEF86D56B
-  L1_2 = L1_2.fA925EE56
-  L1_2 = L1_2()
-  L2_2 = L1_2
-  L1_2 = L1_2.f7A6D9717
-  L3_2 = A0_2[7]
-  L1_2(L2_2, L3_2)
-  L1_2 = cEF86D56B
-  L1_2 = L1_2.fA925EE56
-  L1_2 = L1_2()
-  L2_2 = L1_2
-  L1_2 = L1_2.f79AF9A09
-  L3_2 = false
-  L1_2(L2_2, L3_2)
+  self[7] = 30
+  self[6] = 30
+  self[3] = _hx_tab_array({length = 0}, 0)
+  self[3]:resize(5)
+  cEF86D56B.fA925EE56():f6E91F345(self[6])
+  cEF86D56B.fA925EE56():f7A6D9717(self[7])
+  cEF86D56B.fA925EE56():f79AF9A09(false)
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFD0B7F7DAE438AF7"]
-L69_1 = "__name__"
-L70_1 = "CFD0B7F7DAE438AF7"
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFD0B7F7DAE438AF7"]
-L69_1 = "prototype"
-L70_1 = L15_1
-L70_1 = L70_1()
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFD0B7F7DAE438AF7"]["prototype"]
-L69_1 = "F7EED485852A4D25F"
 
-function L70_1(A0_2)
-  local L1_2, L2_2, L3_2
-  L1_2 = cEF86D56B
-  L1_2 = L1_2.fA925EE56
-  L1_2 = L1_2()
-  L2_2 = L1_2
-  L1_2 = L1_2.f79AF9A09
-  L3_2 = false
-  L1_2(L2_2, L3_2)
+CFD0B7F7DAE438AF7.__name__ = "CFD0B7F7DAE438AF7"
+CFD0B7F7DAE438AF7.prototype = L15_1()
+
+function CFD0B7F7DAE438AF7.prototype.F7EED485852A4D25F(self)  -- Start
+  cEF86D56B.fA925EE56():f79AF9A09(false)
 end
 
 L68_1[L69_1] = L70_1
 L68_1 = _ENV["CFD0B7F7DAE438AF7"]["prototype"]
 L69_1 = "FF204996D13B10446"
 
-function L70_1(A0_2, A1_2, A2_2)
-  local L3_2, L4_2, L5_2, L6_2
-  L3_2 = A0_2[3]
-  L4_2 = L16_1
-  L5_2 = {}
-  L6_2 = {}
-  L6_2.func = true
-  L5_2.__fields__ = L6_2
-  
+function CFD0B7F7DAE438AF7.prototype.FF204996D13B10446(self, A1_2, A2_2)  -- RegisterHandle
+  local L5_2, L6_2
+  L5_2 = {
+      __fields__ = {
+          func = true
+      }
+  }
+
   function L6_2(A0_3, ...)
     local L1_3, L2_3
     L1_3 = A2_2
     L2_3 = ...
     return L1_3(L2_3)
   end
-  
-  L5_2.func = L6_2
-  L4_2 = L4_2(L5_2)
-  L3_2[A1_2] = L4_2
+
+  self[3][A1_2] = L16_1(L5_2)
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFD0B7F7DAE438AF7"]["prototype"]
-L69_1 = _ENV["CFD0B7F7DAE438AF7"]
-L68_1.__class__ = L69_1
+CFD0B7F7DAE438AF7.prototype.__class__ = CFD0B7F7DAE438AF7
