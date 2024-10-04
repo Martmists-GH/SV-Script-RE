a5daa39a0cc7febba12b40e212b787059a93cdfe
Martmists <martmists@gmail.com>
2024-10-04 13:04:38 +0200

    another file
    Signed-off-by: Martmists <martmists@gmail.com>

--- decompiled/CFCD8C9056D1AB38B.lua
+++ decompiled/CFCD8C9056D1AB38B.lua
@@ -1,12 +1,6 @@
-L55_1 = _ENV
-L56_1 = "CFCD8C9056D1AB38B"
-L57_1 = L15_1
-L57_1 = L57_1()
-L55_1[L56_1] = L57_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]
-L69_1 = "new"
+CFCD8C9056D1AB38B = L15_1()
 
-function L70_1(A0_2)
+function CFCD8C9056D1AB38B.new(A0_2)
   local L1_2, L2_2, L3_2, L4_2
   L1_2 = L2_1
   L2_2 = CFCD8C9056D1AB38B
@@ -22,365 +16,118 @@ function L70_1(A0_2)
   return L1_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]
-L69_1 = "super"
-
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2
-  L2_2 = CDC3F92928A2194E6
-  L2_2 = L2_2.super
-  L3_2 = A0_2
-  L4_2 = A1_2
-  L2_2(L3_2, L4_2)
+function CFCD8C9056D1AB38B.super(self, A1_2)
+  CDC3F92928A2194E6.super(self, A1_2)
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = "CFCD8C9056D1AB38B"
-L69_1 = _ENV["CFCD8C9056D1AB38B"]
-L25_1[L68_1] = L69_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]
-L69_1 = "__name__"
-L70_1 = "CFCD8C9056D1AB38B"
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]
-L69_1 = "S731A421D90DEDBF5"
+CFCD8C9056D1AB38B.__name__ = "CFCD8C9056D1AB38B"
 
-function L70_1()
-  local L0_2, L1_2
-  L0_2 = CFCD8C9056D1AB38B
-  L0_2 = L0_2.SA448FFDAA03F13F6
-  return L0_2
+function CFCD8C9056D1AB38B.S731A421D90DEDBF5()  -- get_sceneState
+  return CFCD8C9056D1AB38B.SA448FFDAA03F13F6  -- s_sceneState
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]
-L69_1 = "SC22C2773A68837D3"
-
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2, L5_2, L6_2
-  L2_2 = c682D8E4F
-  L2_2 = L2_2.fEF94D11D
-  L3_2 = A0_2
-  L2_2 = L2_2(L3_2)
-  L3_2 = nil
-  L4_2 = cA042DA13
-  L4_2 = L4_2.fB1E655AE
-  L5_2 = L2_2
-  L6_2 = L3_2
-  L4_2 = L4_2(L5_2, L6_2)
-  if L4_2 then
-    L4_2 = nil
-    return L4_2
+function CFCD8C9056D1AB38B.SC22C2773A68837D3(A0_2, A1_2)  -- SceneLoad
+  local L2_2
+  L2_2 = c682D8E4F.fEF94D11D(A0_2)
+  if cA042DA13.fB1E655AE(L2_2, nil) then
+    return nil
   end
-  L5_2 = L2_2
-  L4_2 = L2_2.f48F8C7FF
-  L4_2 = L4_2(L5_2)
-  if true == L4_2 then
-    L4_2 = nil
-    return L4_2
+  if L2_2:f48F8C7FF() == true then
+    return true
   end
-  L5_2 = L2_2
-  L4_2 = L2_2.f0EF10D0C
-  L4_2(L5_2)
-  L4_2 = CFCD8C9056D1AB38B
-  L4_2.SA448FFDAA03F13F6 = 1
+  L2_2:f0EF10D0C()
+  CFCD8C9056D1AB38B.SA448FFDAA03F13F6 = 1  -- s_sceneState = 1
   if nil ~= A1_2 then
-    L4_2 = CFCD8C9056D1AB38B
-    L4_2.SE8AA57EBB95DA538 = A1_2
+    CFCD8C9056D1AB38B.SE8AA57EBB95DA538 = A1_2  -- s_onEndCallBack = A1_2
   end
   return L2_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]
-L69_1 = "prototype"
-L70_1 = L15_1
-L70_1 = L70_1()
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = "F7C68FEDB79AB6396"
-
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2, L5_2, L6_2, L7_2, L8_2
-  L2_2 = CDC3F92928A2194E6
-  L2_2 = L2_2.prototype
-  L2_2 = L2_2.F7C68FEDB79AB6396
-  L3_2 = A0_2
-  L4_2 = A1_2
-  L2_2(L3_2, L4_2)
-  L2_2 = C1DB14DCC9D7634FA
-  L2_2 = L2_2.new
-  L3_2 = L55_1
-  L4_2 = A0_2
-  L5_2 = A0_2.FF7549BC4EA813FBA
-  L3_2 = L3_2(L4_2, L5_2)
-  L4_2 = L31_1.string
-  L5_2 = L31_1.string
-  L6_2 = ""
-  L5_2 = L5_2(L6_2)
-  L6_2 = L31_1.string
-  L7_2 = A0_2[1]
-  L8_2 = L7_2
-  L7_2 = L7_2.f462C9B70
-  L7_2 = L7_2(L8_2)
-  L8_2 = L7_2
-  L7_2 = L7_2.fE9C29DA1
-  L7_2, L8_2 = L7_2(L8_2)
-  L6_2 = L6_2(L7_2, L8_2)
-  L5_2 = L5_2 .. L6_2
-  L4_2 = L4_2(L5_2)
-  L5_2 = L31_1.string
-  L6_2 = "_scene_setup_sequence"
-  L5_2 = L5_2(L6_2)
-  L4_2 = L4_2 .. L5_2
-  L2_2 = L2_2(L3_2, L4_2)
-  A0_2[2] = L2_2
+function CFCD8C9056D1AB38B.prototype.F7C68FEDB79AB6396(self, A1_2)  -- Setup
+  local L3_2, L4_2
+  CDC3F92928A2194E6.prototype.F7C68FEDB79AB6396(self, A1_2)  -- super.Setup(self, A1_2)
+  L3_2 = L55_1(self, self.FF7549BC4EA813FBA)  -- L55_1(self, self.SetupSequenceBase)
+  L4_2 = L31_1.string(self[1]:f462C9B70():fE9C29DA1())  .. "_scene_setup_sequence"
+  self[2] = C1DB14DCC9D7634FA.new(L3_2, L4_2)  -- ti.Coroutine.new(...)
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = "FE94F3E13286232CF"
-
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2, L5_2, L6_2, L7_2, L8_2, L9_2, L10_2, L11_2
-  L2_2 = A0_2[2]
-  if nil ~= L2_2 then
-    L2_2 = L10_1.coroutine
-    L2_2 = L2_2.status
-    L3_2 = A0_2[2]
-    L3_2 = L3_2[1]
-    L2_2 = L2_2(L3_2)
-    if "dead" ~= L2_2 then
-      L2_2 = A0_2[2]
-      L3_2 = nil
-      L4_2 = L62_1
-      L5_2 = L64_1.pack
-      L6_2 = L10_1.coroutine
-      L6_2 = L6_2.resume
-      L7_2 = L2_2[1]
-      L8_2 = L3_2
-      L6_2, L7_2, L8_2, L9_2, L10_2, L11_2 = L6_2(L7_2, L8_2)
-      L5_2 = L5_2(L6_2, L7_2, L8_2, L9_2, L10_2, L11_2)
-      L6_2 = {}
-      L7_2 = "success"
-      L8_2 = "result"
-      L6_2[1] = L7_2
-      L6_2[2] = L8_2
-      L4_2 = L4_2(L5_2, L6_2)
-      L5_2 = L4_2.success
-      if not L5_2 then
-        L5_2 = C7BD28C2CE195DB4E
-        L5_2 = L5_2.S7989B6DD56823279
-        L6_2 = false
-        L7_2 = L31_1.string
-        L8_2 = L31_1.string
-        L9_2 = L31_1.string
-        L10_2 = "!Error ocurred in coroutine["
-        L9_2 = L9_2(L10_2)
-        L10_2 = L31_1.string
-        L11_2 = L2_2[2]
-        L10_2 = L10_2(L11_2)
-        L9_2 = L9_2 .. L10_2
-        L8_2 = L8_2(L9_2)
-        L9_2 = L31_1.string
-        L10_2 = "]: "
-        L9_2 = L9_2(L10_2)
-        L8_2 = L8_2 .. L9_2
-        L7_2 = L7_2(L8_2)
-        L8_2 = L31_1.string
-        L9_2 = L31_1.string
-        L10_2 = L4_2.result
-        L9_2, L10_2, L11_2 = L9_2(L10_2)
-        L8_2 = L8_2(L9_2, L10_2, L11_2)
-        L7_2 = L7_2 .. L8_2
-        L5_2(L6_2, L7_2)
+function CFCD8C9056D1AB38B.prototype.FE94F3E13286232CF(self, A1_2)  -- PreUpdate
+  local L2_2, L3_2, L4_2, L5_2, L7_2
+  if nil ~= self[2] then
+    if "dead" ~= L10_1.coroutine.status(L3_2) then
+      L5_2 = L64_1.pack(L10_1.coroutine.resume(self[2][1], nil))
+      L4_2 = L62_1(L5_2, {"success", "result"})
+      if not L4_2.success then
+        L7_2 = "!Error ocurred in coroutine["  .. L31_1.string(L2_2[2])  .. "[: " .. L31_1.string(L4_2.result)
+        C7BD28C2CE195DB4E.S7989B6DD56823279(false, L7_2)
       end
     end
   end
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = "F1C2AA00ADAC52EC5"
-
-function L70_1(A0_2)
+function CFCD8C9056D1AB38B.prototype.F1C2AA00ADAC52EC5(self)  -- Destroy
   local L1_2, L2_2
-  L1_2 = CDC3F92928A2194E6
-  L1_2 = L1_2.prototype
-  L1_2 = L1_2.F1C2AA00ADAC52EC5
-  L2_2 = A0_2
-  L1_2(L2_2)
-  L1_2 = CFCD8C9056D1AB38B
-  L1_2 = L1_2.SE8AA57EBB95DA538
-  if nil ~= L1_2 then
-    L1_2 = CFCD8C9056D1AB38B
-    L1_2 = L1_2.SE8AA57EBB95DA538
-    L1_2()
-    L1_2 = CFCD8C9056D1AB38B
-    L1_2.SE8AA57EBB95DA538 = nil
+  CDC3F92928A2194E6.prototype.F1C2AA00ADAC52EC5(self)  -- super.Destroy(self)
+  if nil ~= CFCD8C9056D1AB38B.SE8AA57EBB95DA538 then  -- s_onEndCallBack
+    CFCD8C9056D1AB38B.SE8AA57EBB95DA538()  -- s_onEndCallBack()
+    CFCD8C9056D1AB38B.SE8AA57EBB95DA538 = nil  -- s_onEndCallBack = nil
   end
-  L1_2 = CFCD8C9056D1AB38B
-  L1_2.SA448FFDAA03F13F6 = 0
+  CFCD8C9056D1AB38B.SA448FFDAA03F13F6 = 0  -- s_sceneState = 0
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = "FF7549BC4EA813FBA"
-
-function L70_1(A0_2)
+function CFCD8C9056D1AB38B.prototype.FF7549BC4EA813FBA(self)  -- SetupSequenceBase
   local L1_2, L2_2
-  L2_2 = A0_2
-  L1_2 = A0_2.F155F52A5852449F8
-  L1_2(L2_2)
-  L2_2 = A0_2
-  L1_2 = A0_2.F0E47A597FB435DB7
-  L1_2(L2_2)
-  L1_2 = CFCD8C9056D1AB38B
-  L1_2.SA448FFDAA03F13F6 = 2
+  self:F155F52A5852449F8()  -- self:WaitSceneSetupSequence()
+  self:F0E47A597FB435DB7()  -- self:SetupSequence()
+  CFCD8C9056D1AB38B.SA448FFDAA03F13F6 = 2  -- s_sceneState = 2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = "F0E47A597FB435DB7"
+function CFCD8C9056D1AB38B.prototype.F0E47A597FB435DB7(self)  -- SetupSequence
 
-function L70_1(A0_2)
-  local L1_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = "FB61F64E2486E1EC3"
-
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2, L5_2, L6_2
-  L3_2 = A0_2
-  L2_2 = A0_2.F155F52A5852449F8
-  L2_2(L3_2)
-  L2_2 = A0_2[1]
-  L3_2 = L2_2
-  L2_2 = L2_2.f462C9B70
-  L2_2 = L2_2(L3_2)
-  L3_2 = L2_2
-  L2_2 = L2_2.fD4E64AB7
-  L4_2 = A1_2
-  L2_2 = L2_2(L3_2, L4_2)
-  L3_2 = nil
-  L4_2 = c016374C1
-  L4_2 = L4_2.f4555D276
-  L5_2 = L2_2
-  L6_2 = L3_2
-  L4_2 = L4_2(L5_2, L6_2)
-  if L4_2 then
-    L4_2 = nil
-    return L4_2
+function CFCD8C9056D1AB38B.prototype.FB61F64E2486E1EC3(self, A1_2)  -- GetSceneObjectSequence
+  local L2_2
+  self:F155F52A5852449F8()  -- self:WaitSceneSetupSequence()
+  L2_2 = self[1]:f462C9B70():fD4E64AB7(A1_2)
+  if c016374C1.f4555D276(L2_2, nil) then
+    return nil
   end
   return L2_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = "FD1F2FC5D6118742C"
-
-function L70_1(A0_2, A1_2, A2_2)
-  local L3_2, L4_2, L5_2, L6_2, L7_2, L8_2, L9_2
-  L4_2 = A0_2
-  L3_2 = A0_2.F155F52A5852449F8
-  L3_2(L4_2)
-  L3_2 = nil
-  L4_2 = nil
-  L5_2 = c016374C1
-  L5_2 = L5_2.f8C7D4F4D
-  L6_2 = A1_2
-  L7_2 = L4_2
-  L5_2 = L5_2(L6_2, L7_2)
-  if L5_2 then
+function CFCD8C9056D1AB38B.prototype.FD1F2FC5D6118742C(self, A1_2, A2_2)  -- GetBehaviorSequence
+  local L3_2, L7_2
+  self:F155F52A5852449F8()  -- self:WaitSceneSetupSequence()
+  if c016374C1.f8C7D4F4D(A1_2, nil) then
     L3_2 = A1_2
   end
   if nil ~= A2_2 then
-    L6_2 = A0_2
-    L5_2 = A0_2.FB61F64E2486E1EC3
-    L7_2 = A2_2
-    L5_2 = L5_2(L6_2, L7_2)
-    L3_2 = L5_2
+    L3_2 = self:FB61F64E2486E1EC3(A2_2)  -- self:GetSceneObjectSequence(A2_2)
   end
-  L5_2 = nil
-  L6_2 = c016374C1
-  L6_2 = L6_2.f4555D276
-  L7_2 = L3_2
-  L8_2 = L5_2
-  L6_2 = L6_2(L7_2, L8_2)
-  if L6_2 then
-    L6_2 = nil
-    return L6_2
+  if c016374C1.f4555D276(L3_2, nil) then
+    return nil
   end
-  L6_2 = C9C018BF0431B5277
-  L6_2 = L6_2.S7B6179AB425A98A0
-  L7_2 = L3_2
-  L6_2(L7_2)
-  L7_2 = L3_2
-  L6_2 = L3_2.fB3CF1DEB
-  L6_2 = L6_2(L7_2)
-  L7_2 = C3B091777E3EC94A5
-  L7_2 = L7_2.S3AB27FFAF33EFD2D
-  L7_2 = L7_2.h
-  L7_2 = L7_2[L6_2]
-  L8_2 = L42_1.tnull
-  if L7_2 == L8_2 then
+  C9C018BF0431B5277.S7B6179AB425A98A0(L3_2)  -- main.util.LoaderUtil.WaitSetupSceneObject(L3_2)
+  L7_2 = C3B091777E3EC94A5.S3AB27FFAF33EFD2D.h[L3_2:fB3CF1DEB()]  -- C3B091777E3EC94A5.s_behaviorMap.h[L3_2:fB3CF1DEB()]
+  if L7_2 == L42_1.tnull then
     L7_2 = nil
   end
-  L8_2 = L7_2
-  if nil == L8_2 then
-    L9_2 = nil
-    return L9_2
+  if L7_2 == nil then
+    return nil
   end
-  return L8_2
+  return L7_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = "F155F52A5852449F8"
-
-function L70_1(A0_2)
-  local L1_2, L2_2
-  while true do
-    L1_2 = A0_2[1]
-    L2_2 = L1_2
-    L1_2 = L1_2.f462C9B70
-    L1_2 = L1_2(L2_2)
-    L2_2 = L1_2
-    L1_2 = L1_2.f48F8C7FF
-    L1_2 = L1_2(L2_2)
-    if L1_2 then
-      break
-    end
-    L1_2 = C1DB14DCC9D7634FA
-    L1_2 = L1_2.S760DAE4C5371A78E
-    L1_2()
+function CFCD8C9056D1AB38B.prototype.F155F52A5852449F8(self)  -- WaitSceneSetupSequence
+  while not self[1]:f462C9B70():f48F8C7FF() do
+    C1DB14DCC9D7634FA.S760DAE4C5371A78E()  -- ti.Coroutine.yield()
   end
-  while true do
-    L1_2 = A0_2[1]
-    L2_2 = L1_2
-    L1_2 = L1_2.f462C9B70
-    L1_2 = L1_2(L2_2)
-    L2_2 = L1_2
-    L1_2 = L1_2.f9D8BC178
-    L1_2 = L1_2(L2_2)
-    if L1_2 then
-      break
-    end
-    L1_2 = C1DB14DCC9D7634FA
-    L1_2 = L1_2.S760DAE4C5371A78E
-    L1_2()
+  while not self[1]:f462C9B70():f9D8BC178() do
+    C1DB14DCC9D7634FA.S760DAE4C5371A78E()  -- ti.Coroutine.yield()
   end
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L69_1 = _ENV["CFCD8C9056D1AB38B"]
-L68_1.__class__ = L69_1
-L68_1 = _ENV["CFCD8C9056D1AB38B"]
-L69_1 = "__super__"
-L69_1 = _ENV["CFCD8C9056D1AB38B"]["prototype"]
-L70_1 = {}
-L71_1 = "__index"
+CFCD8C9056D1AB38B.prototype.__class__ = CFCD8C9056D1AB38B
+CFCD8C9056D1AB38B.__super__ = CDC3F92928A2194E6
+setmetatable(CFCD8C9056D1AB38B.prototype, {__index = CDC3F92928A2194E6})
