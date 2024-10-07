b7cb9f6c104d09addad725d9ab170b7571313cca
Martmists <martmists@gmail.com>
2024-10-07 10:07:42 +0200

    main.field.FieldUtility
    Signed-off-by: Martmists <martmists@gmail.com>

--- decompiled/CFC8F368D91411014.lua
+++ decompiled/CFC8F368D91411014.lua
@@ -1,440 +1,177 @@
-L55_1 = _ENV
-L56_1 = "CFC8F368D91411014"
-L57_1 = L15_1
-L57_1 = L57_1()
-L55_1[L56_1] = L57_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "new"
-L70_1 = {}
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "__name__"
-L70_1 = "CFC8F368D91411014"
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "S12AB211B0A58477E"
+---@alias main_field_FieldUtility CFC8F368D91411014
 
-function L70_1()
-  local L0_2, L1_2, L2_2, L3_2
-  L0_2 = nil
-  L1_2 = CFC8F368D91411014
-  L1_2 = L1_2.SDE9EF3CFD428417D
-  if nil ~= L1_2 then
-    L1_2 = L52_1.__cast
-    L2_2 = CFC8F368D91411014
-    L2_2 = L2_2.SDE9EF3CFD428417D
-    L3_2 = C51C789C764D66E76
-    L1_2 = L1_2(L2_2, L3_2)
-    L1_2 = L1_2[2]
-    L2_2 = L1_2
-    L1_2 = L1_2.FEF3A3B6876841F5A
-    L3_2 = C9D8C5D0363DC0E88
-    L1_2 = L1_2(L2_2, L3_2)
-    L0_2 = L1_2
+---@class CFC8F368D91411014
+---@field SDE9EF3CFD428417D main_playables_free_move_ctrl_ActorControlBehavior @ LocalControl
+---@field S693B72BE6E0E949D c016374C1
+---@field S52271CF2FB4598E5 number
+---@field S62509376DBF3DD90 c016374C1
+---@field S57FE4ACC7CEA7DFA any
+---@field S632AA91624A85920 c467D18B0 @ ActiveCamera
+---@field S93A017D496A6D000 any
+---@field S1DE88A67387CAC28 c016374C1 @ FieldCamera
+---@field S117777B4565DA037 any @ FieldCameraComponent
+---@field SF28F48A047888F83 any @ FieldCameraBehaviour
+---@field SC49F2E9D64D921C6 cA042DA13 @ RoomResidentScene
+---@field S944F91EB55F82285 any @ FieldSceneInfo
+CFC8F368D91411014 = L15_1()
+CFC8F368D91411014.new = {}
+CFC8F368D91411014.__name__ = "CFC8F368D91411014"
+
+--- main.field.FieldUtility.S12AB211B0A58477E
+function CFC8F368D91411014.S12AB211B0A58477E()
+  local L0_2
+  if nil ~= CFC8F368D91411014.SDE9EF3CFD428417D then  -- LocalControl
+    local L1_2 = L52_1.__cast(CFC8F368D91411014.SDE9EF3CFD428417D, C51C789C764D66E76)  -- main.playables.free_move.ctrl.ActorControlBehavior
+    L0_2 = L1_2[2]:FEF3A3B6876841F5A(C9D8C5D0363DC0E88)  -- L1_2[2]:Find(C9D8C5D0363DC0E88)
   end
   return L0_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "SBA31B781D8838DE6"
+--- main.field.FieldUtility.SBA31B781D8838DE6
+---@param A0_2 c016374C1
+function CFC8F368D91411014.SBA31B781D8838DE6(A0_2)
+  CFC8F368D91411014.S693B72BE6E0E949D = A0_2
 
-function L70_1(A0_2)
-  local L1_2, L2_2, L3_2, L4_2
-  L1_2 = CFC8F368D91411014
-  L1_2.S693B72BE6E0E949D = A0_2
-  L1_2 = CFC8F368D91411014
-  L1_2 = L1_2.S93A017D496A6D000
-  L2_2 = c016374C1
-  L2_2 = L2_2.f4555D276
-  L3_2 = A0_2
-  
-  function L4_2()
-    local L0_3, L1_3
-    L0_3 = nil
-    L1_3 = L1_2
-    if nil == L1_3 then
-      L0_3 = nil
-    else
-      L0_3 = L1_2.owner
-    end
-    return L0_3
+  local L4_2
+  if nil ~= CFC8F368D91411014.S93A017D496A6D000 then
+    L4_2 = CFC8F368D91411014.S93A017D496A6D000.owner
   end
-  
-  L4_2 = L4_2()
-  L2_2 = L2_2(L3_2, L4_2)
-  if L2_2 then
-    L2_2 = CFC8F368D91411014
-    L2_2.S52271CF2FB4598E5 = 0
-    L2_2 = CFC8F368D91411014
-    L2_2.S62509376DBF3DD90 = nil
-    L2_2 = CFC8F368D91411014
-    L2_2.S57FE4ACC7CEA7DFA = nil
+
+  if c016374C1.f4555D276(A0_2, L4_2) then
+    CFC8F368D91411014.S52271CF2FB4598E5 = 0
+    CFC8F368D91411014.S62509376DBF3DD90 = nil
+    CFC8F368D91411014.S57FE4ACC7CEA7DFA = nil
   else
-    L2_2 = CFC8F368D91411014
-    L2_2.S52271CF2FB4598E5 = 1
-    L2_2 = CFC8F368D91411014
-    L2_2.S62509376DBF3DD90 = A0_2
-    L3_2 = A0_2
-    L2_2 = A0_2.fB3CF1DEB
-    L2_2 = L2_2(L3_2)
-    L3_2 = C3B091777E3EC94A5
-    L3_2 = L3_2.S3AB27FFAF33EFD2D
-    L3_2 = L3_2.h
-    L3_2 = L3_2[L2_2]
-    L4_2 = L42_1.tnull
-    if L3_2 == L4_2 then
+    CFC8F368D91411014.S52271CF2FB4598E5 = 1
+    CFC8F368D91411014.S62509376DBF3DD90 = A0_2
+    local L3_2 = C3B091777E3EC94A5.S3AB27FFAF33EFD2D.h[A0_2:fB3CF1DEB()]
+    if L3_2 == L42_1.tnull then
       L3_2 = nil
     end
-    L4_2 = CFC8F368D91411014
-    L4_2.S57FE4ACC7CEA7DFA = L3_2
+    CFC8F368D91411014.S57FE4ACC7CEA7DFA = L3_2
   end
-  L2_2 = C320308FF98E5E0DA
-  L2_2 = L2_2.SEF74932A742375DF
-  L3_2 = CFC8F368D91411014
-  L3_2 = L3_2.SC942E0202B47E6F3
-  L3_2 = L3_2()
-  L4_2 = CFC8F368D91411014
-  L4_2 = L4_2.S693B72BE6E0E949D
-  L2_2(L3_2, L4_2)
+  C320308FF98E5E0DA.SEF74932A742375DF(CFC8F368D91411014.SC942E0202B47E6F3(), CFC8F368D91411014.S693B72BE6E0E949D)
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "S36AFB231754F9D91"
-
-function L70_1()
-  local L0_2, L1_2, L2_2, L3_2, L4_2, L5_2
-  L0_2 = nil
-  L1_2 = c467D18B0
-  L1_2 = L1_2.fBCDB6533
-  L2_2 = CFC8F368D91411014
-  L2_2 = L2_2.S632AA91624A85920
-  L3_2 = L0_2
-  L1_2 = L1_2(L2_2, L3_2)
-  if not L1_2 then
-    L1_2 = CFC8F368D91411014
-    L1_2 = L1_2.S632AA91624A85920
-    L2_2 = L1_2
-    L1_2 = L1_2.fC14C49EA
-    L1_2 = L1_2(L2_2)
-    if L1_2 then
-      goto lbl_35
+--- main.field.FieldUtility.get_ActiveCamera
+---@return c467D18B0
+function CFC8F368D91411014.S36AFB231754F9D91()
+  if not c467D18B0.fBCDB6533(CFC8F368D91411014.S632AA91624A85920, nil) then  -- ActiveCamera
+    if CFC8F368D91411014.S632AA91624A85920:fC14C49EA() then
+      return CFC8F368D91411014.S632AA91624A85920
     end
   end
-  L1_2 = cAF8D78F3
-  L1_2 = L1_2.f37537CF4
-  L1_2 = L1_2()
-  L2_2 = L1_2
-  L1_2 = L1_2.f426683B4
-  L1_2 = L1_2(L2_2)
-  L2_2 = nil
-  L3_2 = c016374C1
-  L3_2 = L3_2.f8C7D4F4D
-  L4_2 = L1_2
-  L5_2 = L2_2
-  L3_2 = L3_2(L4_2, L5_2)
-  if L3_2 then
-    L3_2 = CFC8F368D91411014
-    L4_2 = c467D18B0
-    L4_2 = L4_2.fB41FD22F
-    L5_2 = L1_2
-    L4_2 = L4_2(L5_2)
-    L3_2.S632AA91624A85920 = L4_2
+  local L1_2 = cAF8D78F3.f37537CF4():f426683B4()
+  if c016374C1.f8C7D4F4D(L1_2, nil) then
+  CFC8F368D91411014.S632AA91624A85920 = c467D18B0.fB41FD22F(L1_2)
   end
-  ::lbl_35::
-  L1_2 = CFC8F368D91411014
-  L1_2 = L1_2.S632AA91624A85920
-  return L1_2
+  return CFC8F368D91411014.S632AA91624A85920
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "SC942E0202B47E6F3"
-
-function L70_1()
-  local L0_2, L1_2, L2_2, L3_2
-  L0_2 = nil
-  L1_2 = c016374C1
-  L1_2 = L1_2.f4555D276
-  L2_2 = CFC8F368D91411014
-  L2_2 = L2_2.S1DE88A67387CAC28
-  L3_2 = L0_2
-  L1_2 = L1_2(L2_2, L3_2)
-  if L1_2 then
-    L1_2 = CFC8F368D91411014
-    L2_2 = CFC8F368D91411014
-    L2_2 = L2_2.S0C6378C0E23B592D
-    L3_2 = 24
-    L2_2 = L2_2(L3_2)
-    L1_2.S1DE88A67387CAC28 = L2_2
+--- main.field.FieldUtility.get_FieldCamera
+---@return c016374C1
+function CFC8F368D91411014.SC942E0202B47E6F3()
+  if c016374C1.f4555D276(CFC8F368D91411014.S1DE88A67387CAC28, nil) then
+    CFC8F368D91411014.S1DE88A67387CAC28 = CFC8F368D91411014.S0C6378C0E23B592D(24)
   end
-  L1_2 = CFC8F368D91411014
-  L1_2 = L1_2.S1DE88A67387CAC28
-  return L1_2
+  return CFC8F368D91411014.S1DE88A67387CAC28
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "S0DE5849509537AAE"
-
-function L70_1()
-  local L0_2, L1_2, L2_2, L3_2, L4_2
-  L0_2 = nil
-  L1_2 = c467D18B0
-  L1_2 = L1_2.fBCDB6533
-  L2_2 = CFC8F368D91411014
-  L2_2 = L2_2.S117777B4565DA037
-  L3_2 = L0_2
-  L1_2 = L1_2(L2_2, L3_2)
-  if L1_2 then
-    L1_2 = nil
-    L2_2 = c016374C1
-    L2_2 = L2_2.f8C7D4F4D
-    L3_2 = CFC8F368D91411014
-    L3_2 = L3_2.SC942E0202B47E6F3
-    L3_2 = L3_2()
-    L4_2 = L1_2
-    L2_2 = L2_2(L3_2, L4_2)
-    if L2_2 then
-      L2_2 = CFC8F368D91411014
-      L2_2 = L2_2.SC942E0202B47E6F3
-      L2_2 = L2_2()
-      L3_2 = L2_2
-      L2_2 = L2_2.f9D8BC178
-      L2_2 = L2_2(L3_2)
-      if L2_2 then
-        L2_2 = CFC8F368D91411014
-        L3_2 = c467D18B0
-        L3_2 = L3_2.fB41FD22F
-        L4_2 = CFC8F368D91411014
-        L4_2 = L4_2.SC942E0202B47E6F3
-        L4_2 = L4_2()
-        L3_2 = L3_2(L4_2)
-        L2_2.S117777B4565DA037 = L3_2
+--- main.field.FieldUtility.get_FieldCameraComponent
+function CFC8F368D91411014.S0DE5849509537AAE()
+  if c467D18B0.fBCDB6533(CFC8F368D91411014.S117777B4565DA037, nil) then
+    if c016374C1.f8C7D4F4D(CFC8F368D91411014.SC942E0202B47E6F3(), nil) then
+      if CFC8F368D91411014.SC942E0202B47E6F3():f9D8BC178() then
+        CFC8F368D91411014.S117777B4565DA037 = c467D18B0.fB41FD22F(CFC8F368D91411014.SC942E0202B47E6F3())
       end
     end
   end
-  L1_2 = CFC8F368D91411014
-  L1_2 = L1_2.S117777B4565DA037
-  return L1_2
+  return CFC8F368D91411014.S117777B4565DA037
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "SEBF4ADF2F268C1A6"
-
-function L70_1()
-  local L0_2, L1_2, L2_2, L3_2
-  L0_2 = CFC8F368D91411014
-  L0_2 = L0_2.SF28F48A047888F83
-  if nil == L0_2 then
-    L0_2 = nil
-    L1_2 = c016374C1
-    L1_2 = L1_2.f8C7D4F4D
-    L2_2 = CFC8F368D91411014
-    L2_2 = L2_2.SC942E0202B47E6F3
-    L2_2 = L2_2()
-    L3_2 = L0_2
-    L1_2 = L1_2(L2_2, L3_2)
-    if L1_2 then
-      L1_2 = CFC8F368D91411014
-      L1_2 = L1_2.SC942E0202B47E6F3
-      L1_2 = L1_2()
-      L2_2 = L1_2
-      L1_2 = L1_2.fB3CF1DEB
-      L1_2 = L1_2(L2_2)
-      L2_2 = C3B091777E3EC94A5
-      L2_2 = L2_2.S3AB27FFAF33EFD2D
-      L2_2 = L2_2.h
-      L2_2 = L2_2[L1_2]
-      L3_2 = L42_1.tnull
-      if L2_2 == L3_2 then
+--- main.field.FieldUtility.get_FieldCameraBehaviour
+function CFC8F368D91411014.SEBF4ADF2F268C1A6()
+  if nil == CFC8F368D91411014.SF28F48A047888F83 then
+    if c016374C1.f8C7D4F4D(CFC8F368D91411014.SC942E0202B47E6F3(), nil) then
+      local L1_2 = CFC8F368D91411014.SC942E0202B47E6F3():fB3CF1DEB()
+      local L2_2 = C3B091777E3EC94A5.S3AB27FFAF33EFD2D.h[L1_2]
+      if L2_2 == L42_1.tnull then
         L2_2 = nil
       end
-      L3_2 = CFC8F368D91411014
-      L3_2.SF28F48A047888F83 = L2_2
+      CFC8F368D91411014.SF28F48A047888F83 = L2_2
     end
   end
-  L0_2 = CFC8F368D91411014
-  L0_2 = L0_2.SF28F48A047888F83
-  return L0_2
+  return CFC8F368D91411014.SF28F48A047888F83
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "S81CFAE7959A0B46F"
-
-function L70_1()
-  local L0_2, L1_2, L2_2, L3_2
-  L0_2 = nil
-  L1_2 = c016374C1
-  L1_2 = L1_2.f8C7D4F4D
-  L2_2 = CFC8F368D91411014
-  L2_2 = L2_2.S0C6378C0E23B592D
-  L3_2 = 25
-  L2_2 = L2_2(L3_2)
-  L3_2 = L0_2
-  L1_2 = L1_2(L2_2, L3_2)
-  if L1_2 then
-    L1_2 = CFC8F368D91411014
-    L1_2 = L1_2.S0C6378C0E23B592D
-    L2_2 = 25
-    L1_2 = L1_2(L2_2)
-    L2_2 = L1_2
-    L1_2 = L1_2.fB3CF1DEB
-    L1_2 = L1_2(L2_2)
-    L2_2 = C3B091777E3EC94A5
-    L2_2 = L2_2.S3AB27FFAF33EFD2D
-    L2_2 = L2_2.h
-    L2_2 = L2_2[L1_2]
-    L3_2 = L42_1.tnull
-    if L2_2 == L3_2 then
+--- main.field.FieldUtility.S81CFAE7959A0B46F
+function CFC8F368D91411014.S81CFAE7959A0B46F()
+  if c016374C1.f8C7D4F4D(CFC8F368D91411014.S0C6378C0E23B592D(25), nil) then
+    local L1_2 = CFC8F368D91411014.S0C6378C0E23B592D(25):fB3CF1DEB(L2_2)
+    local L2_2 = C3B091777E3EC94A5.S3AB27FFAF33EFD2D.h[L1_2]
+    if L2_2 == L42_1.tnull then
       L2_2 = nil
     end
     return L2_2
   end
-  L1_2 = nil
-  return L1_2
+  return nil
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "SFE40ADAE3FCBFCD1"
-
-function L70_1()
-  local L0_2, L1_2, L2_2, L3_2
-  L0_2 = nil
-  L1_2 = cA042DA13
-  L1_2 = L1_2.fB1E655AE
-  L2_2 = CFC8F368D91411014
-  L2_2 = L2_2.SC49F2E9D64D921C6
-  L3_2 = L0_2
-  L1_2 = L1_2(L2_2, L3_2)
-  if L1_2 then
-    L1_2 = CFC8F368D91411014
-    L2_2 = c682D8E4F
-    L2_2 = L2_2.fEF94D11D
-    L3_2 = "resident_room"
-    L2_2 = L2_2(L3_2)
-    L1_2.SC49F2E9D64D921C6 = L2_2
+--- main.field.FieldUtility.get_RoomResidentScene()
+---@return cA042DA13
+function CFC8F368D91411014.SFE40ADAE3FCBFCD1()
+  if cA042DA13.fB1E655AE(CFC8F368D91411014.SC49F2E9D64D921C6, nil) then
+    CFC8F368D91411014.SC49F2E9D64D921C6 = c682D8E4F.fEF94D11D("resident_room")
   end
-  L1_2 = CFC8F368D91411014
-  L1_2 = L1_2.SC49F2E9D64D921C6
-  return L1_2
+  return CFC8F368D91411014.SC49F2E9D64D921C6
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "S474A279FC41C6D90"
-
-function L70_1()
-  local L0_2, L1_2
-  L0_2 = CFC8F368D91411014
-  L0_2 = L0_2.S944F91EB55F82285
-  if nil == L0_2 then
-    L0_2 = CFC8F368D91411014
-    L1_2 = L15_1
-    L1_2 = L1_2()
-    L0_2.S944F91EB55F82285 = L1_2
+--- main.field.FieldUtility.get_FieldSceneInfo()
+function CFC8F368D91411014.S474A279FC41C6D90()
+  if nil == CFC8F368D91411014.S944F91EB55F82285 then
+    CFC8F368D91411014.S944F91EB55F82285 = L15_1()
   end
-  L0_2 = CFC8F368D91411014
-  L0_2 = L0_2.S944F91EB55F82285
-  return L0_2
+  return CFC8F368D91411014.S944F91EB55F82285
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "S0C6378C0E23B592D"
-
-function L70_1(A0_2)
-  local L1_2, L2_2, L3_2, L4_2, L5_2, L6_2, L7_2, L8_2
-  L1_2 = CF67A67452D018ECC
-  L1_2 = L1_2.SC8223E31D3163519
-  L2_2 = nil
-  L3_2 = L1_2[7]
-  L3_2 = L3_2.h
-  L4_2 = false
+--- main.field.FieldUtility.FindFieldObject
+---@param A0_2 number behaviourType
+function CFC8F368D91411014.S0C6378C0E23B592D(A0_2)
+  local L3_2 = CF67A67452D018ECC.SC8223E31D3163519[7].h
+  local L2_2
   while nil ~= L3_2 do
-    L5_2 = L3_2.item
+    local L5_2 = L3_2.item
     L3_2 = L3_2.next
-    L7_2 = L5_2
-    L6_2 = L5_2.F6C886FC694510481
-    L6_2 = L6_2(L7_2)
-    if not L6_2 then
-      L6_2 = L1_2[7]
-      L7_2 = L6_2
-      L6_2 = L6_2.remove
-      L8_2 = L5_2
-      L6_2(L7_2, L8_2)
+    if not L5_2:F6C886FC694510481() then  -- IsValid
+      CF67A67452D018ECC.SC8223E31D3163519[7]:remove(L5_2)
     else
-      L7_2 = L5_2
-      L6_2 = L5_2.FBDA175393973D042
-      L6_2 = L6_2(L7_2)
-      if L6_2 == A0_2 then
+      if L5_2:FBDA175393973D042() == A0_2 then  -- GetBehaviourType
         L2_2 = L5_2
-        L4_2 = true
-      else
+        break
       end
     end
-    if L4_2 then
-      L4_2 = false
-      break
-    end
   end
-  L5_2 = L2_2
-  if nil ~= L5_2 then
-    L6_2 = L5_2[7]
+  if nil ~= L2_2 then
+    local L6_2 = L2_2[7]
     if nil == L6_2 then
-      L7_2 = nil
-      return L7_2
+      return nil
     else
-      L7_2 = L6_2.owner
-      return L7_2
+      return L6_2.owner
     end
   end
-  L6_2 = nil
-  return L6_2
+  return nil
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["CFC8F368D91411014"]
-L69_1 = "S190FDCAC5247C3AF"
-
-function L70_1()
-  local L0_2, L1_2, L2_2, L3_2, L4_2, L5_2, L6_2
-  L0_2 = C2DE996446A3F15D2
-  L0_2 = L0_2.S4C492863970293DA
-  L0_2 = L0_2()
+--- main.field.FieldUtility.S190FDCAC5247C3AF
+function CFC8F368D91411014.S190FDCAC5247C3AF()
+  local L0_2 = C2DE996446A3F15D2.S4C492863970293DA()
   if L0_2 then
-    L1_2 = c2A8846F6
-    L1_2 = L1_2.f3E170423
-    L1_2 = L1_2()
-    if L1_2 then
-      L1_2 = 0
-      while L1_2 < 4 do
-        L1_2 = L1_2 + 1
-        L2_2 = L1_2 - 1
-        L3_2 = nil
-        L4_2 = c59FF1887
-        L4_2 = L4_2.fB7D3B54A
-        L5_2 = cE9AFE246
-        L5_2 = L5_2.f8EBF28C9
-        L6_2 = L2_2
-        L5_2 = L5_2(L6_2)
-        L6_2 = L3_2
-        L4_2 = L4_2(L5_2, L6_2)
-        if L4_2 then
-          L4_2 = CF1D074B6B410BDB8
-          L4_2 = L4_2.S0F5FFD6E81C0BEDA
-          L5_2 = L2_2
-          L4_2 = L4_2(L5_2)
-          if 1 == L4_2 then
-            L4_2 = CF1D074B6B410BDB8
-            L4_2 = L4_2.S9EEDC69BBCEE9B0F
-            L5_2 = L2_2
-            L4_2 = L4_2(L5_2)
-            L0_2 = 0 ~= L4_2
+    if c2A8846F6.f3E170423() then
+      for L1_2 = 0, 3 do
+        local L5_2 = cE9AFE246.f8EBF28C9(L1_2 - 1)
+        if c59FF1887.fB7D3B54A(L5_2, nil) then
+          if 1 == CF1D074B6B410BDB8.S0F5FFD6E81C0BEDA(L1_2 - 1) then
+            L0_2 = 0 ~= CF1D074B6B410BDB8.S9EEDC69BBCEE9B0F(L1_2 - 1)
             if not L0_2 then
               break
             end
@@ -445,5 +182,3 @@ function L70_1()
   end
   return L0_2
 end
-
-L68_1[L69_1] = L70_1
