d51e1f654eabf909df5036f9f78264e6949b67e1
Martmists <martmists@gmail.com>
2024-10-09 17:38:15 +0200

    main.ui.hud_notice.data.HudNoticeDataAccessor

--- decompiled/C759AABB8037E728E.lua
+++ decompiled/C759AABB8037E728E.lua
@@ -1,12 +1,11 @@
-L55_1 = _ENV
-L56_1 = "C759AABB8037E728E"
-L57_1 = L15_1
-L57_1 = L57_1()
-L55_1[L56_1] = L57_1
-L68_1 = _ENV["C759AABB8037E728E"]
-L69_1 = "new"
+---@alias main_ui_hud_notice_data_HudNoticeDataAccessor C759AABB8037E728E
 
-function L70_1(A0_2)
+---@class C759AABB8037E728E : C759AABB8037E728E_prototype
+---@field prototype C759AABB8037E728E_prototype
+C759AABB8037E728E = L15_1()
+
+---@param A0_2 cF52F390B
+function C759AABB8037E728E.new(A0_2)
   local L1_2, L2_2, L3_2, L4_2
   L1_2 = L2_1
   L2_2 = C759AABB8037E728E
@@ -22,174 +21,83 @@ function L70_1(A0_2)
   return L1_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]
-L69_1 = "super"
-
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2
-  A0_2[2] = nil
-  L2_2 = _hx_tab_array
-  L3_2 = {}
-  L3_2.length = 0
-  L4_2 = 0
-  L2_2 = L2_2(L3_2, L4_2)
-  A0_2[1] = L2_2
-  A0_2[2] = A1_2
+---@param self C759AABB8037E728E
+---@param A1_2 cF52F390B
+function C759AABB8037E728E.super(self, A1_2)
+  self[1] = _hx_tab_array({length = 0}, 0)
+  self[2] = A1_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]
-L69_1 = "__name__"
-L70_1 = "C759AABB8037E728E"
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]
-L69_1 = "prototype"
-L70_1 = L15_1
-L70_1 = L70_1()
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]["prototype"]
-L69_1 = "F96BE4A383B32C1B7"
+C759AABB8037E728E.__name__ = "C759AABB8037E728E"
+---@class C759AABB8037E728E_prototype
+---@field [1] hx_tab_array<C759AABB8037E728E_anon_1>
+---@field [2] cF52F390B
+C759AABB8037E728E_prototype = L15_1()
+C759AABB8037E728E.prototype = C759AABB8037E728E_prototype
 
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2
-  L3_2 = A0_2
-  L2_2 = A0_2.F1E3C77B8049F40D4
-  L4_2 = A1_2
-  L2_2 = L2_2(L3_2, L4_2)
+--- main.ui.hud_notice.data.HudNoticeDataAccessor.GetDataById
+function C759AABB8037E728E_prototype:F96BE4A383B32C1B7(A1_2)
+  local L2_2 = self:F1E3C77B8049F40D4(A1_2)
   if -1 == L2_2 then
-    L3_2 = nil
-    return L3_2
+    return nil
   end
-  L3_2 = A0_2[1]
-  L3_2 = L3_2[L2_2]
-  return L3_2
+  return self[1][L2_2]
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]["prototype"]
-L69_1 = "F1E3C77B8049F40D4"
-
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2, L5_2
-  L2_2 = 0
-  L3_2 = A0_2[1]
-  L3_2 = L3_2.length
-  while L2_2 < L3_2 do
-    L2_2 = L2_2 + 1
-    L4_2 = L2_2 - 1
-    L5_2 = A0_2[1]
-    L5_2 = L5_2[L4_2]
-    L5_2 = L5_2.ID
-    if L5_2 == A1_2 then
-      return L4_2
+--- main.ui.hud_notice.data.HudNoticeDataAccessor.F1E3C77B8049F40D4
+function C759AABB8037E728E_prototype:F1E3C77B8049F40D4(A1_2)
+  for L2_2 = 0, self[1].length - 1 do
+    if self[1][L2_2].ID == A1_2 then
+      return L2_2
     end
   end
-  L4_2 = -1
-  return L4_2
+  return -1
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]["prototype"]
-L69_1 = "F584AD3ED067D371F"
-
-function L70_1(A0_2)
-  local L1_2, L2_2, L3_2, L4_2, L5_2, L6_2, L7_2, L8_2, L9_2, L10_2
-  L1_2 = cF52F390B
-  L1_2 = L1_2.fB41FD22F
-  L2_2 = A0_2[2]
-  L1_2 = L1_2(L2_2)
-  L2_2 = L1_2
-  L1_2 = L1_2.f287946D6
-  L1_2 = L1_2(L2_2)
-  L2_2 = 0
-  L4_2 = L1_2
-  L3_2 = L1_2.f6902A503
-  L5_2 = "values"
-  L3_2 = L3_2(L4_2, L5_2)
-  while L2_2 < L3_2 do
-    L2_2 = L2_2 + 1
-    L4_2 = L2_2 - 1
-    L6_2 = A0_2
-    L5_2 = A0_2.F80B5D892ADA38C8B
-    L8_2 = L1_2
-    L7_2 = L1_2.f0CA5FEBC
-    L9_2 = "values"
-    L10_2 = L4_2
-    L7_2 = L7_2(L8_2, L9_2, L10_2)
-    L8_2 = L4_2
-    L5_2(L6_2, L7_2, L8_2)
+--- main.ui.hud_notice.data.HudNoticeDataAccessor.F584AD3ED067D371F
+function C759AABB8037E728E_prototype:F584AD3ED067D371F()
+  local L1_2 = cF52F390B.fB41FD22F(self[2]):f287946D6()
+  for L2_2 = 0, L1_2:f6902A503("values") - 1 do
+    self:F80B5D892ADA38C8B(L1_2:f0CA5FEBC("values", L2_2), L2_2)  -- setData
   end
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]["prototype"]
-L69_1 = "F80B5D892ADA38C8B"
-
-function L70_1(A0_2, A1_2, A2_2)
-  local L3_2, L4_2, L5_2
-  L4_2 = A0_2
-  L3_2 = A0_2.F18FFA1904F2051CE
-  L5_2 = A1_2
-  L3_2 = L3_2(L4_2, L5_2)
-  L4_2 = A0_2[1]
-  L4_2[A2_2] = L3_2
+--- main.ui.hud_notice.data.HudNoticeDataAccessor.setData
+function C759AABB8037E728E_prototype:F80B5D892ADA38C8B(A1_2, A2_2)
+  self[1][A2_2] = self:F18FFA1904F2051CE(A1_2)
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]["prototype"]
-L69_1 = "F18FFA1904F2051CE"
-
-function L70_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2, L5_2
-  L3_2 = A0_2
-  L2_2 = A0_2.FCE76AAAB92EC5028
-  L2_2 = L2_2(L3_2)
-  L4_2 = A1_2
-  L3_2 = A1_2.fCD31E312
-  L5_2 = "ID"
-  L3_2 = L3_2(L4_2, L5_2)
-  L2_2.ID = L3_2
-  L4_2 = A1_2
-  L3_2 = A1_2.fCD31E312
-  L5_2 = "iconType"
-  L3_2 = L3_2(L4_2, L5_2)
-  L2_2.IconType = L3_2
-  L4_2 = A1_2
-  L3_2 = A1_2.f3D9D438D
-  L5_2 = "mstxtLabel"
-  L3_2 = L3_2(L4_2, L5_2)
-  L2_2.MstxtLabel = L3_2
-  L4_2 = A1_2
-  L3_2 = A1_2.f6DAE9B28
-  L5_2 = "IsBBMissionName"
-  L3_2 = L3_2(L4_2, L5_2)
-  L2_2.MstxtFileIsBBMission = L3_2
+--- main.ui.hud_notice.data.HudNoticeDataAccessor.F18FFA1904F2051CE
+function C759AABB8037E728E_prototype:F18FFA1904F2051CE(A1_2)
+  local L2_2 = self:FCE76AAAB92EC5028()  -- getDefaultData
+  L2_2.ID = A1_2:fCD31E312("ID")
+  L2_2.IconType = A1_2:fCD31E312("iconType")
+  L2_2.MstxtLabel = A1_2:f3D9D438D("mstxtLabel")
+  L2_2.MstxtFileIsBBMission = A1_2:f6DAE9B28("IsBBMissionName")
   return L2_2
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]["prototype"]
-L69_1 = "FCE76AAAB92EC5028"
-
-function L70_1(A0_2)
-  local L1_2, L2_2, L3_2
-  L1_2 = L16_1
-  L2_2 = {}
-  L3_2 = {}
-  L3_2.ID = true
-  L3_2.IconType = true
-  L3_2.MstxtLabel = true
-  L3_2.MstxtFileIsBBMission = true
-  L2_2.__fields__ = L3_2
-  L2_2.ID = 0
-  L2_2.IconType = 0
-  L2_2.MstxtLabel = ""
-  L2_2.MstxtFileIsBBMission = false
-  return L1_2(L2_2)
+--- main.ui.hud_notice.data.HudNoticeDataAccessor.getDefaultData
+---@return C759AABB8037E728E_anon_1
+function C759AABB8037E728E_prototype:F18FFA1904F2051CE()
+  return L16_1 {
+    __fields__ = {
+      ID = true,
+      IconType = true,
+      MstxtLabel = true,
+      MstxtFileIsBBMission = true,
+    },
+    ID = 0,
+    IconType = 0,
+    MstxtLabel = "",
+    MstxtFileIsBBMission = false
+  }
 end
 
-L68_1[L69_1] = L70_1
-L68_1 = _ENV["C759AABB8037E728E"]["prototype"]
-L69_1 = _ENV["C759AABB8037E728E"]
-L68_1.__class__ = L69_1
+C759AABB8037E728E_prototype.__class__ = C759AABB8037E728E
+
+---@class C759AABB8037E728E_anon_1
+---@field ID number
+---@field IconType number
+---@field MstxtLabel string
+---@field MstxtFileIsBBMission boolean
